c32272334887ffdc2e77c45fc7880569b70bc0fc
Andrej Sh. <126242006+AndrejFinki@users.noreply.github.com>
2024-11-30 10:49:40 +0000

    Fix multiple issues
    Remove venv sed command
    
    Use venv instead of local python due to other issue KafkaProducer
    
    Add sleep inbetween actions

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 python -m venv ./venv
+sleep 3
+. ./venv/bin/activate
+sleep 3
 pip install -r requirements.txt
+sleep 3
 docker-compose up -d
+sleep 3
+sed -i '/from kafka.vendor.six.moves import range/d' venv/lib/python3.12/site-packages/kafka/codec.py
+sleep 3
 python produce_messages.py
